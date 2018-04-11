4f833570e1f6badbaf5e3e135db9a8be83d7976e
Olivier Riccini <olivier-webdev@hotmail.com>
2018-04-11 14:58:32 -0400

    change db relationship

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,8 @@
 class Document < ApplicationRecord
   belongs_to :user
-  has_many :transactions
+
+  has_many :transactions, dependent: :destroy
+  has_many :documents, through: :transactions
 
   validates :type, presence: true
 end
