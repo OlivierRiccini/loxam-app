7d04bf660faadfe7302218ca124b56a5c4d25340
Olivier Riccini <olivier-webdev@hotmail.com>
2018-04-15 18:53:51 -0400

    db relationship fixed

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   belongs_to :user
 
   has_many :transactions, dependent: :destroy
-  has_many :documents, through: :transactions
+  has_many :products, through: :transactions
 
   validates :document_type, presence: true
 end
