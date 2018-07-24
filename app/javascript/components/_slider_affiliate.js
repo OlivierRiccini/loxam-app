12b099c1079935541bcd0d0907459ad12973464e
Olivier Riccini <olivier-webdev@hotmail.com>
2018-07-24 15:30:05 +0200

    slider working exit

--- app/javascript/components/_slider_affiliate.js
+++ app/javascript/components/_slider_affiliate.js
@@ -1,10 +1,29 @@
-$("#slider-affiliate-images > img:gt(0)").hide();
+// $( document ).ready(function() {
+  // $("#slider-affiliate-images > img:gt(0)").hide();
+  // setInterval(function() {
+  //   $('#slider-affiliate-images > img:first')
+  //     .fadeOut(2000)
+  //     .next()
+  //     .fadeIn(3000)
+  //     .end()
+  //     .appendTo('#slider-affiliate-images');
+  // }, 4000);
+// }
 
-setInterval(function() {
-  $('#slider-affiliate-images > img:first')
-    .fadeOut(2000)
-    .next()
-    .fadeIn(3000)
-    .end()
-    .appendTo('#slider-affiliate-images');
-},  4000);
+const images = document.querySelectorAll('#slider-affiliate-images img');
+
+let i = 0;
+let j = -1;
+
+function slider() {
+  jQuery(images[i++]).fadeIn(2000);
+  jQuery(images[j++]).fadeOut(3000);
+  if (i > images.length - 1) {
+    i = 0;
+  };
+  if (j > images.length - 1) {
+    j = 0;
+  };
+}
+
+setInterval(slider, 3000);
