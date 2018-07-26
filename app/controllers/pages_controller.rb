8861694e919720c803af5bea46e978e38c7964ea
Olivier Riccini <olivier-webdev@hotmail.com>
2018-07-26 16:59:23 +0200

    no duplicated invoices

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -183,20 +183,26 @@ class PagesController < ApplicationController
 
       text = File.open(fname).read
       text.each_line do |line|
-        array = line.strip.split(/\;/)
+        file_order_columns = line.strip.split(/\;/)
 
-        unless User.where(loxam_id: array[2]).exists?
-          User.create(name: array[3], email: Faker::Internet.email,
-                    password: Faker::IDNumber.valid, loxam_id: array[2])
+        unless User.where(loxam_id: file_order_columns[2]).exists?
+          User.create(name: file_order_columns[3], email: Faker::Internet.email,
+                    password: Faker::IDNumber.valid, loxam_id: file_order_columns[2])
         end
 
         files_pdf.each do |pdf_doc|
-          if pdf_doc == array[6]
-          p "#{pdf_doc} == #{array[6]}"
+          # file_order_columns[6] == pdf id
+          if pdf_doc == file_order_columns[6]
+          puts "#{pdf_doc} == #{file_order_columns[6]}"
             ftp.getbinaryfile(pdf_doc, pdf_doc)
-            new_doc = Invoice.new(id_invoice_loxam: array[0], user_id: User.where(loxam_id: array[2]).take.id)
-            new_doc.remote_pdf_url = pdf_doc
-            new_doc.save
+
+            user = User.where(loxam_id: file_order_columns[2]).take
+            unless user.invoices.any? { |invoice| invoice[:id_invoice_loxam] == file_order_columns[0].to_i }
+              new_doc = Invoice.new(id_invoice_loxam: file_order_columns[0], user_id: user.id)
+              new_doc.remote_pdf_url = pdf_doc
+              new_doc.save
+            end
+
             File.delete(pdf_doc)
           end
         end
