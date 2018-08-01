9abe3145add0e7bf991e04e33c5eada4e69957e0
Olivier Riccini <olivier-webdev@hotmail.com>
2018-08-01 17:37:16 +0200

    map done

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :content, length: { minimum: 20, maximum: 500 }
+  validates :content, length: { minimum: 10, maximum: 500 }
 
   private
 
