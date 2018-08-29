90e8b4677af980d4669b89ec111e8856acffd84b
Olivier Riccini <olivier-webdev@hotmail.com>
2018-08-29 12:19:31 +0200

    creating webhook

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-
+  before_action :get_headers
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -7,9 +7,14 @@ class UserMailer < ApplicationMailer
   #
   def welcome(user)
     @user = user
-    mail('X-Postmark-Account-Token' => 'd9d5b157-c617-4500-8195-f4cffde890fd',
-         from: 'contact@loxambastia.com',
+    mail(from: 'contact@loxambastia.com',
          to: @user.email,
          subject: 'Bienvenu chez Loxam Bastia!')
+  end
+
+  private
+
+  def get_headers
+    headers['X-Postmark-Account-Token'] = 'd9d5b157-c617-4500-8195-f4cffde890fd',
   end
 end
