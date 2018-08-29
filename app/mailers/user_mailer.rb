cfafdb17e6f7d11752c5f9b888a1bdafa64541ca
Olivier Riccini <olivier-webdev@hotmail.com>
2018-08-29 12:07:17 +0200

    creating webhook

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,15 @@ class UserMailer < ApplicationMailer
   # with the following lookup:
   #
   #   en.user_mailer.welcome.subject
-  #
+  #    mail.headers['X-Postmark-Account-Token'] = 'd9d5b157-c617-4500-8195-f4cffde890fd'
+
   def welcome(user)
     @user = user
-    mail(from: 'contact@loxambastia.com',
-         to: @user.email,
-         subject: 'Bienvenu chez Loxam Bastia!')
+    mail(headers = {
+                      'X-Postmark-Account-Token' = 'd9d5b157-c617-4500-8195-f4cffde890fd'
+                    }
+                    from: 'contact@loxambastia.com',
+                    to: @user.email,
+                    subject: 'Bienvenu chez Loxam Bastia!')
   end
 end
