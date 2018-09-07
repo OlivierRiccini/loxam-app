eb1f98e9f68075fbc3c2511ba63f62c987fb2691
Olivier Riccini <olivier-webdev@hotmail.com>
2018-09-07 18:03:08 +0200

    fixed gibbon bug

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
 Gibbon::Request.api_key = Rails.application.secrets.mailchimp_api_key
-Gibbon::Request.api_key = Rails.application.secrets.mailchimp_list_id
+Gibbon::Request.list_id = Rails.application.secrets.mailchimp_list_id
 Gibbon::Request.timeout = 15
